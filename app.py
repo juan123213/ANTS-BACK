5e44101588545a537333ec4a9f7e8861afb2bd55
Juan Andrés <juan.andresgiraldo@hotmail.com>
2023-09-29 09:38:58 -0500

    conexión a la base de datos firebase

--- app.py
+++ app.py
@@ -1,3 +1,40 @@
 from fastapi import FastAPI
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+
+# Carga las credenciales desde el archivo JSON que descargaste
+cred = credentials.Certificate("ants-97.json")
+
+# Inicializa la aplicación Firebase con las credenciales
+firebase_app = firebase_admin.initialize_app(cred, {
+        "databaseURL": "https://ants-978b7-default-rtdb.firebaseio.com",
+})
+
+ref = db.reference("/Usuario")
+
+ref.set({
+    
+    'Usuario1':
+        {
+            'Nombre': 'Antonio',
+            'Edad': 23,
+            'Correo': 'juan.giraldo@gmail.com',
+            'Telefono': '987654321',
+            'Direccion': 'Calle 123',
+        }
+    
+})
+
+data = ref.get()
+
+print(data)
+
+
 
 app = FastAPI()
+
+# Define una ruta que devuelva los datos almacenados en la variable "data"
+@app.get("/datos-usuario/")
+async def obtener_datos_usuario():
+    return data
